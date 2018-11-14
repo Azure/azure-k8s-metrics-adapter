edd44117d2e826ed93e59fba8caea06572f97d89
Lee Cattarin <lee.cattarin@gmail.com>
2018-11-14 10:40:15 -0800

    Update the service bus sample to work with sb API changes

--- samples/servicebus-queue/consumer/main.go
+++ samples/servicebus-queue/consumer/main.go
@@ -29,26 +29,25 @@ func main() {
 	}
 
 	fmt.Println("setting up listener")
-	listenHandle, err := q.Receive(context.Background(),
-		func(ctx context.Context, msg *servicebus.Message) servicebus.DispositionAction {
-			fmt.Println("received message: ", string(msg.Data))
+	var messageHandler servicebus.HandlerFunc = func(ctx context.Context, msg *servicebus.Message) servicebus.DispositionAction {
+		fmt.Println("received message: ", string(msg.Data))
 
-			ctx, cancel := context.WithTimeout(context.Background(), 20*time.Second)
-			defer cancel()
+		ctx, cancel := context.WithTimeout(context.Background(), 20*time.Second)
+		defer cancel()
 
-			qe, err := qm.Get(ctx, queueName)
-			if err != nil {
-				fmt.Println("create manager created error: ", err)
-			}
-			fmt.Println("number message left: ", *qe.MessageCount)
-			return msg.Complete()
-		})
+		qe, err := qm.Get(ctx, queueName)
+		if err != nil {
+			fmt.Println("create manager created error: ", err)
+		}
+		fmt.Println("number message left: ", *qe.MessageCount)
+		return msg.Complete()
+	}
+
+	err = q.Receive(context.Background(), messageHandler)
 	if err != nil {
 		// handle queue listener creation err
 		fmt.Println("listener: ", err)
 	}
-	// Close the listener handle for the Service Bus Queue
-	defer listenHandle.Close(context.Background())
 
 	fmt.Println("listening...")
 
