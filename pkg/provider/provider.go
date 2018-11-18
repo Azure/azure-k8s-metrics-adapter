5a29f7b3dddb0f0ced777222ce3afdf31415b28f
Marc Sensenich <sensenichm91@gmail.com>
2018-11-18 13:10:59 +0000

    [GH-42] Add the Service Bus Client to the Provider

--- pkg/provider/provider.go
+++ pkg/provider/provider.go
@@ -4,32 +4,32 @@ package provider
 
 import (
 	"github.com/Azure/azure-k8s-metrics-adapter/pkg/azure/appinsights"
-	"github.com/Azure/azure-k8s-metrics-adapter/pkg/metriccache"
-
 	"github.com/Azure/azure-k8s-metrics-adapter/pkg/azure/monitor"
-
+	"github.com/Azure/azure-k8s-metrics-adapter/pkg/azure/servicebus"
+	"github.com/Azure/azure-k8s-metrics-adapter/pkg/metriccache"
+	"github.com/kubernetes-incubator/custom-metrics-apiserver/pkg/provider"
 	apimeta "k8s.io/apimachinery/pkg/api/meta"
 	"k8s.io/client-go/dynamic"
-
-	"github.com/kubernetes-incubator/custom-metrics-apiserver/pkg/provider"
 )
 
 type AzureProvider struct {
-	appinsightsClient     appinsights.AzureAppInsightsClient
-	mapper                apimeta.RESTMapper
-	kubeClient            dynamic.Interface
-	monitorClient         monitor.AzureMonitorClient
-	metricCache           *metriccache.MetricCache
-	defaultSubscriptionID string
+	appinsightsClient            appinsights.AzureAppInsightsClient
+	mapper                       apimeta.RESTMapper
+	kubeClient                   dynamic.Interface
+	monitorClient                monitor.AzureMonitorClient
+	metricCache                  *metriccache.MetricCache
+	serviceBusSubscriptionClient servicebus.AzureServiceBusSubscriptionClient
+	defaultSubscriptionID        string
 }
 
-func NewAzureProvider(defaultSubscriptionID string, mapper apimeta.RESTMapper, kubeClient dynamic.Interface, appinsightsClient appinsights.AzureAppInsightsClient, monitorClient monitor.AzureMonitorClient, metricCache *metriccache.MetricCache) provider.MetricsProvider {
+func NewAzureProvider(defaultSubscriptionID string, mapper apimeta.RESTMapper, kubeClient dynamic.Interface, appinsightsClient appinsights.AzureAppInsightsClient, monitorClient monitor.AzureMonitorClient, serviceBusSubscriptionClient servicebus.AzureServiceBusSubscriptionClient, metricCache *metriccache.MetricCache) provider.MetricsProvider {
 	return &AzureProvider{
-		defaultSubscriptionID: defaultSubscriptionID,
-		mapper:                mapper,
-		kubeClient:            kubeClient,
-		appinsightsClient:     appinsightsClient,
-		monitorClient:         monitorClient,
-		metricCache:           metricCache,
+		defaultSubscriptionID:        defaultSubscriptionID,
+		mapper:                       mapper,
+		kubeClient:                   kubeClient,
+		appinsightsClient:            appinsightsClient,
+		monitorClient:                monitorClient,
+		metricCache:                  metricCache,
+		serviceBusSubscriptionClient: serviceBusSubscriptionClient,
 	}
 }
