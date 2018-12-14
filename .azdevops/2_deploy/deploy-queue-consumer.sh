461652fabd25261d3baed458261f0561b942c118
Lee Cattarin <lee.cattarin@gmail.com>
2018-12-14 14:18:41 -0800

    Fix error in queue consumer script

--- .azdevops/2_deploy/deploy-queue-consumer.sh
+++ .azdevops/2_deploy/deploy-queue-consumer.sh
@@ -12,5 +12,5 @@ fi
 
 echo; echo "Deploying queue consumer..."
 cd $GOPATH/src/github.com/Azure/azure-k8s-metrics-adapter/samples/servicebus-queue/
-kubectl create secret generic servicebuskey --from-literal=sb-connection-string=$(SERVICEBUS_CONNECTION_STRING)
+kubectl create secret generic servicebuskey --from-literal=sb-connection-string=$SERVICEBUS_CONNECTION_STRING
 kubectl apply -f deploy/consumer-deployment.yaml
