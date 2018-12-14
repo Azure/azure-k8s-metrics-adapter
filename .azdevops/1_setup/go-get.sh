8bad8587f369a3494118d9e4f693ff422d7344ad
Lee Cattarin <lee.cattarin@gmail.com>
2018-12-14 12:33:37 -0800

    Typo fix in go get

--- .azdevops/1_setup/go-get.sh
+++ .azdevops/1_setup/go-get.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-go get -dv github.com/Azure/azure-k8s-metrics-adapter
-go get -dv github.com/kubernetes-incubator/metrics-server/...
+go get -d github.com/Azure/azure-k8s-metrics-adapter
+go get -d github.com/kubernetes-incubator/metrics-server/...
 
 cd $GOPATH/src/github.com/Azure/azure-k8s-metrics-adapter/samples/servicebus-queue
 go get -u github.com/Azure/azure-service-bus-go
