be7512939d80754c87b43825e584cd88c9877b21
Lee Cattarin <lee.cattarin@gmail.com>
2018-12-14 12:45:47 -0800

    Attempt to fix go errors

--- .azdevops/1_setup/go-get.sh
+++ .azdevops/1_setup/go-get.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+echo "Getting metrics adapter code..."
 go get -d github.com/Azure/azure-k8s-metrics-adapter
+echo "Getting metrics server code..."
 go get -d github.com/kubernetes-incubator/metrics-server/...
 
+echo "Getting go service bus library..."
 cd $GOPATH/src/github.com/Azure/azure-k8s-metrics-adapter/samples/servicebus-queue
 go get -u github.com/Azure/azure-service-bus-go
