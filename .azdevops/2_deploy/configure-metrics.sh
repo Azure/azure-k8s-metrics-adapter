be40a47a072d2dd6c8c01814a894b44be48a85da
Lee Cattarin <lee.cattarin@gmail.com>
2018-12-14 12:57:35 -0800

    Switch scripts to require external GOPATH env var

--- .azdevops/2_deploy/configure-metrics.sh
+++ .azdevops/2_deploy/configure-metrics.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [[ ! -v GOPATH ]]; then
+	echo "Must set GOPATH (/home/vsts/go on Azure Pipelines)"
+	exit 1
+fi
+
 echo "Configuring external metric (queuemessages)..."
 cd $GOPATH/src/github.com/Azure/azure-k8s-metrics-adapter/samples/servicebus-queue/
 sed -i 's|sb-external-ns|'$(SERVICEBUS_NS)'|g' deploy/externalmetric.yaml
