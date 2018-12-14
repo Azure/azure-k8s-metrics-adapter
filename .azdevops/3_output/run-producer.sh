8566b7dd3f10b905dd907ade9041f71d7fbdfaa8
Lee Cattarin <lee.cattarin@gmail.com>
2018-12-14 14:53:02 -0800

    Typo fix in path

--- .azdevops/3_output/run-producer.sh
+++ .azdevops/3_output/run-producer.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo; echo "Building producer..."
-cd $GOPATH?src/github.com/Azure/azure-k8s-metrics-adapter/samples/servicebus-queue/
+cd $GOPATH/src/github.com/Azure/azure-k8s-metrics-adapter/samples/servicebus-queue/
 make
 
 echo; echo "Running producer..."
