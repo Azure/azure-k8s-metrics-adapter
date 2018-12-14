351b94b8cfd5988c13e144cb33f00ed8595dd8a0
Lee Cattarin <lee.cattarin@gmail.com>
2018-12-14 15:34:30 -0800

    Fix error with SB resource group & namespace

--- .azdevops/2_deploy/configure-metrics.sh
+++ .azdevops/2_deploy/configure-metrics.sh
@@ -13,4 +13,5 @@ fi
 echo; echo "Configuring external metric (queuemessages)..."
 cd $GOPATH/src/github.com/Azure/azure-k8s-metrics-adapter/samples/servicebus-queue/
 sed -i 's|sb-external-ns|'${SERVICEBUS_NAMESPACE}'|g' deploy/externalmetric.yaml
+sed -i 's|sb-external-example|'${SERVICEBUS_RESOURCEGROUP}'|g' deploy/externalmetric.yaml
 kubectl apply -f deploy/externalmetric.yaml
