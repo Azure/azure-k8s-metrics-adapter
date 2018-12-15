acb8c34f8635f703661003bf2dedfe9d1ae77152
Lee Cattarin <lee.cattarin@gmail.com>
2018-12-14 17:06:58 -0800

    Removed buggy line

--- .azdevops/3_output/check-metrics-endpoint.sh
+++ .azdevops/3_output/check-metrics-endpoint.sh
@@ -2,6 +2,3 @@
 
 echo; echo "Checking metrics endpoint..."
 kubectl get --raw "/apis/external.metrics.k8s.io/v1beta1" | jq .
-
-echo; echo
-kubectl  get --raw "/apis/external.metrics.k8s.io/v1beta1/namespaces/default/queuemessages" | jq .
