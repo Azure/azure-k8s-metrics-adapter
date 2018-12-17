f283968fb220e1aa6ed9ca7c0842756a97e24479
Lee Cattarin <lee.cattarin@gmail.com>
2018-12-17 10:17:00 -0800

    Add catch for timeout completion

--- .azdevops/3_output/check-hpa.sh
+++ .azdevops/3_output/check-hpa.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
 echo; echo "Showing HPA pod data for 5 min..."
-timeout 300s kubectl get hpa consumer-scaler -w
+timeout 600s kubectl get hpa consumer-scaler -w
+
+# 124 is the exit status if the command times out rather than completing when using 
+# `timeout`. Since this one can't complete and we just want to let it run for some
+# time, timing out is successful operation
+if [ $? = 124 ]; then
+    exit 0
+fi
