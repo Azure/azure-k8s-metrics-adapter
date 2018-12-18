14a2523dbc670f5ce3b5a725f78a480069a45929
Lee Cattarin <lee.cattarin@gmail.com>
2018-12-17 17:28:42 -0800

    Change to loop of kubectl cmd instead of timeout

--- .azdevops/3_output/check-hpa.sh
+++ .azdevops/3_output/check-hpa.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
-echo; echo "Showing HPA pod data for 5 min..."
-timeout 600s kubectl get hpa consumer-scaler -w
-
-# 124 is the exit status if the command times out rather than completing when using 
-# `timeout`. Since this one can't complete and we just want to let it run for some
-# time, timing out is successful operation
-if [ $? = 124 ]; then
-    exit 0
-fi
+echo; echo "Showing HPA pod data for 10 min..."
+START=`date +%s`
+while [ $(( $(date +%s) - 600 )) -lt $START ]; do
+    kubectl get hpa consumer-scaler
+    sleep 15
+done
