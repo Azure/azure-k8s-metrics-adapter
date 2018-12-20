7035afe9a7f549da5f07701c27e26d121d650b6a
Lee Cattarin <lee.cattarin@gmail.com>
2018-12-19 16:08:12 -0800

    Typo fix - amd65 -> amd64

--- .azdevops/0_install/install-helm.sh
+++ .azdevops/0_install/install-helm.sh
@@ -8,5 +8,5 @@ fi
 echo; echo "Installing helm..."
 curl -Lo helm.tar.gz https://storage.googleapis.com/kubernetes-helm/helm-v$HELM_VERSION-linux-amd64.tar.gz
 tar -xvf helm.tar.gz
-sudo mv linux-amd65/helm /usr/local/bin/helm
+sudo mv linux-amd64 /helm /usr/local/bin/helm
 rm -rf linux-amd64 helm.tar.gz
