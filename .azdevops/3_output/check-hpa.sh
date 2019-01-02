ec4be9f0103b9935409eb7ac13f13c1c64b19b5b
Lee Cattarin <lee.cattarin@gmail.com>
2019-01-02 12:21:25 -0800

    Change time window for hpa output

--- .azdevops/3_output/check-hpa.sh
+++ .azdevops/3_output/check-hpa.sh
@@ -3,7 +3,7 @@
 echo; echo "Showing HPA pod data for 10 min..."
 kubectl get hpa consumer-scaler
 START=`date +%s`
-while [ $(( $(date +%s) - 545 )) -lt $START ]; do
+while [ $(( $(date +%s) - 285 )) -lt $START ]; do
     sleep 15
     kubectl get hpa consumer-scaler --no-headers
 done
