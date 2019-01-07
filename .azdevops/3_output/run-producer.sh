440b331c92f01c79dcdfeadd8307a7df954c53c1
Lee Cattarin <lee.cattarin@gmail.com>
2019-01-07 15:53:29 -0800

    Change number of messages made by producer

--- .azdevops/3_output/run-producer.sh
+++ .azdevops/3_output/run-producer.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
-echo; echo "Building producer..."
 cd $GOPATH/src/github.com/Azure/azure-k8s-metrics-adapter/samples/servicebus-queue/
+
+echo; echo "Creating random number for producer script..."
+NUM = (( ( RANDOM % 30 )  + 1 ))
+sed -i 's|20000|'${NUM}'|g' producer/main.go
+
+echo; echo "Building producer..."
 make
 
-echo; echo "Running producer..."
+echo; echo "Sending $NUM messages..."
 ./bin/producer 0 > /dev/null
