26469543ca746a5b28ad2af0d0f0a59b8f03590f
Lee Cattarin <lee.cattarin@gmail.com>
2019-01-07 16:07:51 -0800

    Fix typo in random number generation

--- .azdevops/3_output/run-producer.sh
+++ .azdevops/3_output/run-producer.sh
@@ -3,7 +3,7 @@
 cd $GOPATH/src/github.com/Azure/azure-k8s-metrics-adapter/samples/servicebus-queue/
 
 echo; echo "Creating random number for producer script..."
-NUM = (( ( RANDOM % 30 )  + 1 ))
+NUM=$(( ($RANDOM % 30 )  + 1 ))
 sed -i 's|20000|'${NUM}'|g' producer/main.go
 
 echo; echo "Building producer..."
