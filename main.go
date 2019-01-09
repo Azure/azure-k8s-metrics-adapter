a7bdd38fadcc34c5bdaab2a50d24b3cfd1f3c1e6
Lee Cattarin <lee.cattarin@gmail.com>
2019-01-09 10:34:18 -0800

    Add test log output

--- main.go
+++ main.go
@@ -100,6 +100,8 @@ func getDefaultSubscriptionID() string {
 	// if the user explicitly sets we should use that
 	subscriptionID := os.Getenv("SUBSCRIPTION_ID")
 	if subscriptionID == "" {
+		// TODO remove after testing
+		glog.Errorf("Didn't find SUBSCRIPTION_ID env var")
 		//fallback to trying azure instance meta data
 		azureConfig, err := instancemetadata.GetAzureConfig()
 		if err != nil {
