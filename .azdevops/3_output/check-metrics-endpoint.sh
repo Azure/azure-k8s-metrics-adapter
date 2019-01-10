0c4329ad57610b215cbaf4444961e0d4a014e09d
Lee Cattarin <lee.cattarin@gmail.com>
2019-01-09 16:58:11 -0800

    Change MSGCOUNT expression

--- .azdevops/3_output/check-metrics-endpoint.sh
+++ .azdevops/3_output/check-metrics-endpoint.sh
@@ -2,10 +2,10 @@
 
 echo; echo "Checking metrics endpoint..."
 
-MSGCOUNT = kubectl get --raw "/apis/external.metrics.k8s.io/v1beta1/namespaces/default/queuemessages" | jq .items[0].value
+MSGCOUNT=$(kubectl get --raw "/apis/external.metrics.k8s.io/v1beta1/namespaces/default/queuemessages" | jq .items[0].value)
 while [[ "$MSGCOUNT" == "\"0\"" ]]; do
   sleep 15
-  MSGCOUNT = kubectl get --raw "/apis/external.metrics.k8s.io/v1beta1/namespaces/default/queuemessages" | jq .items[0].value
+  MSGCOUNT=$(kubectl get --raw "/apis/external.metrics.k8s.io/v1beta1/namespaces/default/queuemessages" | jq .items[0].value)
 done
 
 kubectl  get --raw "/apis/external.metrics.k8s.io/v1beta1/namespaces/default/queuemessages" | jq .
