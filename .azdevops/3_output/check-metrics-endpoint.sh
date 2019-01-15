c065612b5240391d57934362a094fa778c4053d2
Lee Cattarin <lee.cattarin@gmail.com>
2019-01-15 10:21:11 -0800

    Switch back to while loop for metric check

--- .azdevops/3_output/check-metrics-endpoint.sh
+++ .azdevops/3_output/check-metrics-endpoint.sh
@@ -2,10 +2,11 @@
 
 echo; echo "Checking metrics endpoint..."
 
-#MSGCOUNT=$(kubectl get --raw "/apis/external.metrics.k8s.io/v1beta1/namespaces/default/queuemessages" | jq .items[0].value)
-#while [[ "$MSGCOUNT" == "\"0\"" ]]; do
-#  sleep 15
-#  MSGCOUNT=$(kubectl get --raw "/apis/external.metrics.k8s.io/v1beta1/namespaces/default/queuemessages" | jq .items[0].value)
-#done
-sleep 120
-kubectl  get --raw "/apis/external.metrics.k8s.io/v1beta1/namespaces/default/queuemessages" | jq .
+MSGCOUNT=$(kubectl get --raw "/apis/external.metrics.k8s.io/v1beta1/namespaces/default/queuemessages" | jq .items[0].value)
+while [[ "$MSGCOUNT" == "\"0\"" ]]; do
+  sleep 15
+  MSGCOUNT=$(kubectl get --raw "/apis/external.metrics.k8s.io/v1beta1/namespaces/default/queuemessages" | jq .items[0].value)
+done
+
+#sleep 120
+#kubectl  get --raw "/apis/external.metrics.k8s.io/v1beta1/namespaces/default/queuemessages" | jq .
