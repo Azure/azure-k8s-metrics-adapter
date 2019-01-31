fa10cabd9a6943c76289904490762f9145a25a37
Lee Cattarin <lee.cattarin@gmail.com>
2019-01-31 15:27:37 -0800

    Add cleaner timer and better error output

--- .azdevops/2_deploy/deploy-adapter-with-sp.sh
+++ .azdevops/2_deploy/deploy-adapter-with-sp.sh
@@ -14,13 +14,15 @@ helm install --name adapter \
 echo; echo "Waiting for deployment to be available..."
 START=`date +%s`
 
+kubectl get deploy/adapter-azure-k8s-metrics-adapter
 while [[ ! `kubectl get deploy/adapter-azure-k8s-metrics-adapter -o jsonpath="{@.status.availableReplicas}"` == 1 ]] && \
         [[ $(( $(date +%s) - 105 )) -lt $START ]]; do 
-    kubectl get deploy/adapter-azure-k8s-metrics-adapter
     sleep 15
+    kubectl get deploy/adapter-azure-k8s-metrics-adapter --no-headers
 done
 
 if [[ ! `kubectl get deploy/adapter-azure-k8s-metrics-adapter -o jsonpath="{@.status.availableReplicas}"` == 1 ]]; then
     kubectl describe deploy/adapter-azure-k8s-metrics-adapter
+    echo
     kubectl logs deploy/adapter-azure-k8s-metrics-adapter
 fi
