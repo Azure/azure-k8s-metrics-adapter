a16056446c2442ad3c36ad930e0d0a61e8e72f87
Lee Cattarin <lee.cattarin@gmail.com>
2019-02-01 15:47:55 -0800

    Make replacing message count re-runnable, replace == w/ =

--- .azdevops/3_output/gen-and-check-messages.sh
+++ .azdevops/3_output/gen-and-check-messages.sh
@@ -19,22 +19,25 @@ sed -i 's|externalq|'${SERVICEBUS_QUEUE_NAME}'|g' producer/main.go
 echo; echo "Building producer and consumer..."
 make
 
+# Re-add the '20000' value to make this repeatable
+sed -i 's|'$(( NUM + 1 ))'|20000|g' producer/main.go
+
 echo; echo "Sending $NUM messages..."
 ./bin/producer 0 > /dev/null
 
-echo; echo "Checking metrics endpoint..."
+echo; echo "Checking metrics endpoint for 4 minutes..."
 
 MSGCOUNT=$(kubectl get --raw "/apis/external.metrics.k8s.io/v1beta1/namespaces/default/queuemessages" | jq .items[0].value)
 START=`date +%s`
 
-while [[ ! "$MSGCOUNT" == "\"$NUM\"" ]] && [[ $(( $(date +%s) - 225 )) -lt $START ]]; do
+while [[ ! "$MSGCOUNT" = "\"$NUM\"" ]] && [[ $(( $(date +%s) - 220 )) -lt $START ]]; do
   sleep 20
   MSGCOUNT=$(kubectl get --raw "/apis/external.metrics.k8s.io/v1beta1/namespaces/default/queuemessages" | jq .items[0].value)
   echo "Endpoint returned $MSGCOUNT messages"
 done
 
-if [[ ! "$MSGCOUNT" == "\"$NUM\"" ]]; then
-    echo "Message count ($MSGCOUNT) not equal to number of messages sent ($NUM)"
+if [[ ! "$MSGCOUNT" = "\"$NUM\"" ]]; then
+    echo "Timed out, message count ($MSGCOUNT) not equal to number of messages sent ($NUM)"
     exit 1
 else
     echo "Message count equal to number of messages sent, metrics adapter working correctly"
