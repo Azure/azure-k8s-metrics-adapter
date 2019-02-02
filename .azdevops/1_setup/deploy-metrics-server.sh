863ed20905cdbaa984c83f00c093ac50bed09b5a
Lee Cattarin <lee.cattarin@gmail.com>
2019-02-01 19:15:50 -0800

    test removal of metrics server deployment

--- .azdevops/1_setup/deploy-metrics-server.sh
+++ .azdevops/1_setup/deploy-metrics-server.sh
@@ -1,10 +1,2 @@
 #!/bin/bash
 
-set -o nounset
-set -o errexit
-
-GOPATH="${GOPATH:-$HOME/go}"
-
-echo; echo "Deploying metrics server (for k8s v1.8+)..."
-cd $GOPATH/src/github.com/kubernetes-incubator/metrics-server/
-kubectl create -f deploy/1.8+/
--- .azdevops/1_setup/go-get.sh
+++ .azdevops/1_setup/go-get.sh
@@ -5,9 +5,7 @@ set -o errexit
 
 GOPATH="${GOPATH:-$HOME/go}"
 
-echo; echo "Getting metrics server code..."
-go get -d github.com/kubernetes-incubator/metrics-server/...
-
 echo; echo "Getting go service bus library..."
 cd $GOPATH/src/github.com/Azure/azure-k8s-metrics-adapter/samples/servicebus-queue
 go get -u github.com/Azure/azure-service-bus-go
+ 
