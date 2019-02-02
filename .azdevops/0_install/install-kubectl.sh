a2da0a619a7a43d549ffe3ad7f1f63237c1da784
Lee Cattarin <lee.cattarin@gmail.com>
2019-02-01 17:09:06 -0800

    Add checksums to downloaded installations

--- .azdevops/0_install/install-kubectl.sh
+++ .azdevops/0_install/install-kubectl.sh
@@ -4,6 +4,8 @@ set -o nounset
 set -o errexit
 
 echo; echo "Installing kubectl..."
-curl -Lo kubectl https://storage.googleapis.com/kubernetes-release/release/v$KUBERNETES_VERSION/bin/linux/amd64/kubectl
-chmod +x kubectl
-sudo mv kubectl /usr/local/bin
+sudo apt-get update && sudo apt-get install -y apt-transport-https
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
+sudo apt-get update
+sudo apt-get install -y kubectl
--- .azdevops/0_install/install-minikube.sh
+++ .azdevops/0_install/install-minikube.sh
@@ -5,5 +5,10 @@ set -o errexit
 
 echo; echo "Installing minikube..."
 curl -Lo minikube https://storage.googleapis.com/minikube/releases/v$MINIKUBE_VERSION/minikube-linux-amd64
+curl -Lo minikube.sha256 https://storage.googleapis.com/minikube/releases/v$MINIKUBE_VERSION/minikube-linux-amd64.sha256
+
+expected=$(cat minikube.sha256) 
+echo "$expected minikube" | sha256sum -c
+
 chmod +x minikube
 sudo mv minikube /usr/local/bin
